08baceec337b17d30e6fe0493e6612b490bb0e15
Dana Harris <optikool@yahoo.com>
2018-09-29 17:40:55 -0700

    Refactored code

--- app/app.module.js
+++ app/app.module.js
@@ -1,23 +1,27 @@
 import angular from 'angular';
+import animate from 'angular-animate';
+import bootstrap from 'angular-ui-bootstrap';
 import uirouter from 'angular-ui-router';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'jquery';
 import 'popper.js';
-import 'bootstrap';
+// import 'bootstrap';
 
 // import 'css/custom.scss';
 
 import routes from './app.routes';
-import home from './home'
-import instructions from './instructions';
+import home from './pages/home'
+import instructions from './pages/instructions';
 
 import componentModule from './components';
 import servicesModule from './services';
 
 export default angular.module('boilerplatePoc', [
         uirouter,
+        animate,
+        bootstrap,
         home,
         instructions,
         componentModule,
--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -4,7 +4,7 @@ import uirouter from 'angular-ui-router';
 import './home.less';
 
 import routes from './home.routes';
-import HomeService from '../services/home.service'
+import HomeService from '../../services/home.service'
 
 export default angular.module('boilerplatePoc.home', [uirouter])
     .config(routes)
