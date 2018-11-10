177a58acd7c3b9b4324ad1f7d8a3b6c4f8c90dd6
Dana Harris <optikool@yahoo.com>
2018-11-10 01:05:04 -0800

    Code cleanup

--- src/app/actions/index.js
+++
@@ -1,5 +0,0 @@
-import angular from 'angular';
-import { combineReducers } from 'redux';
-import CollectionsReducer from './collections.reducer';
-
-export default angular.module('boilerplatePoc.actions', []);
